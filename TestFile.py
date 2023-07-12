54660d45f20e4c92b97a00ca630df52cf21504ad
Nikhilkumar0708 <Nikhilkumar07.1994@gmail.com>
2023-07-12 20:20:10 +0530

    python

--- TestFile.py
+++ TestFile.py
@@ -1,19 +1,10 @@
-print ("Hello ! Hell0")
-print (45)
-
-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+import time
 
-driver = webdriver.Firefox()
+driver = webdriver.Chrome()
 driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element(By.NAME, "q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
-driver.close()
+time.sleep(30)
 
 
